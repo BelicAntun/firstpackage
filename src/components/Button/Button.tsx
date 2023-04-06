d6128b2af34f64c7f040692964cb0ff040c7c175
Antun Belic <antun.belic@init.hr>
2023-04-06 14:30:21 +0200

    add infinitey loop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function Button({ children }: { children: React.ReactNode | string }) {
-  return <button>{children}</button>
+  return (
+    <button
+      onClick={() => {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          alert('U HAVE BEEN HACKED SUCKER!!!!!')
+        }
+      }}
+    >
+      {children}
+    </button>
+  )
 }
