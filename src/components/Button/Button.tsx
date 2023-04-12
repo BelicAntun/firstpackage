694d9c2cfb6ce78aebdc55c25963df856b61b888
Antun Belic <antun.belic@init.hr>
2023-04-12 09:35:37 +0200

    (package):rewrite

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,5 @@
 import React from 'react'
 
-export default function Button({ children }: { children: React.ReactNode | string }) {
-  return (
-    <button
-      onClick={() => {
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
-          alert('U HAVE BEEN HACKED SUCKER!!!!!')
-        }
-      }}
-    >
-      {children}
-    </button>
-  )
+export const Button = ({ children }: { children: React.ReactNode | string }) => {
+  return <button>{children}</button>
 }
